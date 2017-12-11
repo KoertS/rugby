104f5a0c422de099a9c7d65b2b8bf690e4d67aec
Koert Schreurs <kschreurs@ru.nl>
2017-12-11 22:43:51 +0100

    update schedulegetter. app now works with most recent schedule(2017/2018)

--- app/src/main/java/rugbynl/rugbynl/MainActivity.java
+++ app/src/main/java/rugbynl/rugbynl/MainActivity.java
@@ -1,17 +1,28 @@
 package rugbynl.rugbynl;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    String url;
-    String urlObelix1 = "http://www.erugby.nl/pub/nrb/2017-2018/2e_Klasse_Heren_Zuid/index.htm";
-    String urlObelix2 = "http://www.erugby.nl/pub/nrb/2017-2018/4e_Klasse_Heren_Zuid_-_Oost/index.htm";
+    private String urlObelix1 = "http://www.erugby.nl/pub/nrb/2017-2018/2e_Klasse_Heren_Zuid/index.htm";
+    private String urlObelix2 = "http://www.erugby.nl/pub/nrb/2017-2018/4e_Klasse_Heren_Zuid_-_Oost/index.htm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +41,21 @@ public class MainActivity extends AppCompatActivity {
         obelix1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url = urlObelix1;
-                List<Match> matches = scheduleGetter.getSchedule(urlObelix1, "obelix");
-                System.out.print(matches.get(0).getHome());
+                String[] scheduleParams = {urlObelix1, "obelix"};
+                new getSchedule().execute(scheduleParams);
             }
         });
 
         obelix2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                url = urlObelix2;
-                List<Match> matches = scheduleGetter.getSchedule(urlObelix2, "obelix");
-                System.out.print(matches.get(0).getHome());
+                String[] scheduleParams = {urlObelix2, "obelix"};
+                new getSchedule().execute(scheduleParams);
             }
         });
 
     }
-/*
+
     public void fillTable(List<Match> matches) {
         // http://stackoverflow.com/questions/18207470/adding-table-rows-dynamically-in-android
         TableLayout stk = (TableLayout) findViewById(R.id.table_main);
@@ -70,18 +79,13 @@ public class MainActivity extends AppCompatActivity {
         tbrow0.addView(tv4);
         stk.addView(tbrow0);
 
-        for (int i = 1; i < matches.size(); i+=2) { // fill table
+        for (Match match : matches) { // fill table
             TableRow tbrow = new TableRow(this);
-
-        //    String dateRaw = matches.get(i-1).select("td").text();
-            String[] dateSplit = dateRaw.split(" ");
-            String date = dateSplit[1] + " " + dateSplit[2].substring(0,3) + ".";
-       //     Element row = matches.get(i);
-            Elements cols = row.select("td");
-            String time = cols.get(0).text();
-            String home = shorten(cols.get(2).text());
-            String guest = shorten(cols.get(3).text());
-            String score = cols.get(4).text();
+            String date = match.getDate();
+            String time = match.getTime();
+            String home = match.getHome();
+            String guest = match.getGuest();
+            String score = match.getScore();
 
             TextView t1v = new TextView(this);
             t1v.setText(space(date));
@@ -108,9 +112,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-*/
+
     /**
      * Places spaces around string
+     *
      * @param s, string
      * @return " " + string + " "
      */
@@ -118,20 +123,123 @@ public class MainActivity extends AppCompatActivity {
         return " " + s + " ";
     }
 
+
+
+
     /**
-     * Shortens a string if above certain length
-     * @param s, string
-     * @return string shorten then maxLength
+     * Retrieves schedule. String params: url, team
      */
-    private String shorten(String s) {
-        int maxLength=13;
-        if(s.length() < maxLength) return s;
-        s = s.substring(0, maxLength);
-        return s;
+    private class getSchedule extends AsyncTask<String, Void, Void> {
+        private String title;
+        private ProgressDialog dialog;
+        private List<Match> matches;
+
+        public getSchedule() {
+            dialog = new ProgressDialog(MainActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create (loading) dialog
+            dialog.setMessage("Loading...");
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String url = params[0];
+            String team = params[1];
+            try {
+                // Connect to the website
+                Document doc = Jsoup.connect(url).get();
+                // Get the html document title
+                title = doc.title();
+
+                // http://stackoverflow.com/questions/24772828/how-to-parse-html-table-using-jsoup
+                Elements table = doc.select(".results"); // select the table with schedule and results
+                Elements rows = table.select("tr");
+
+                matches = addTeamMatches(team, rows);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        private List<Integer> getHeaderIndexes(Elements rows) {
+            List<Integer> headerIndex = new ArrayList<>();
+            for (int i = 0; i < rows.size(); i++) {
+                if (rows.get(i).select("td").size() < 2) { // if collum size is 1 then it is header
+                    headerIndex.add(i);
+                }
+            }
+            return headerIndex;
+        }
+
+        private List<Match> addTeamMatches(String team, Elements rows) {
+            List<Integer> headerIndexes = getHeaderIndexes(rows);
+            List<Match> matches = new ArrayList<>();
+            for (int i = 0; i < rows.size(); i++) { // loop through all matches
+                if (rows.get(i).text().toLowerCase().contains(team)) { // only pick matches that contain team
+                    Match match = new Match();
+                    int indexOfHeader = getMatchingHeader(i, headerIndexes);
+
+                    String dateString = rows.get(indexOfHeader).select("td").text();
+                    String[] dateSplit = dateString.split(" ");
+
+                    String day = String.format("%02d", Integer.parseInt(dateSplit[1])); // force length to 2 by adding 0 in front
+                    String month = dateSplit[2];
+                    String year = dateSplit[3];
+                    String date = day + " " + month + " " + year;
+                    match.setDate(date);
+
+                    Elements cols = rows.get(i).select("td");
+                    match.setTime(cols.get(0).text());
+                    match.setHome(cols.get(2).text());
+                    match.setGuest(cols.get(3).text());
+                    match.setScore(cols.get(4).text());
+
+                    matches.add(match);
+                }
+            }
+            return matches;
+        }
+
+        /**
+         * This function returns first smaller number in list given a number
+         *
+         * @param number        Integer
+         * @param headerIndexes List<Integer>
+         * @return first number that is smaller than the given number
+         */
+        private int getMatchingHeader(int number, List<Integer> headerIndexes) {
+            for (int i = headerIndexes.size() - 1; i > -1; i--) {
+                if (headerIndexes.get(i) < number) {
+                    return headerIndexes.get(i);
+                }
+            }
+            return -1;
+        }
+
+        @Override
+        protected void onPostExecute(Void results) {
+            // Display result:
+            // Set title into TextView
+            TextView titleTextView = (TextView) findViewById(R.id.title_textview);
+            titleTextView.setText(title);
+            System.out.print(matches.get(0).getHome());
+            // fill table
+            fillTable(matches);
+
+
+            // Close dialog
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+        }
     }
-
-
-
-
 
 }
--- app/src/main/java/rugbynl/rugbynl/ScheduleGetter.java
+++ app/src/main/java/rugbynl/rugbynl/ScheduleGetter.java
@@ -1,19 +1,10 @@
 package rugbynl.rugbynl;
 
-import android.os.AsyncTask;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by ASE on 6-9-17.
  */
-// Title AsyncTask
 public class ScheduleGetter {
     private List<Match> matches;
     private String team;
@@ -22,106 +13,9 @@ public class ScheduleGetter {
     public List<Match> getSchedule(String url, String team){
         this.team = team;
         this.url = url;
-        new getScheduleTask();
+       // new getScheduleTask();
         return matches;
     }
 
-    private class getScheduleTask extends AsyncTask<Void, Void, Void>{
-        private String title;
 
-        public getScheduleTask() {
-            super();
-        }
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            // Create (loading) dialog
-
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                // Connect to the website
-                Document doc = Jsoup.connect(url).get();
-                // Get the html document title
-                title = doc.title();
-
-                // http://stackoverflow.com/questions/24772828/how-to-parse-html-table-using-jsoup
-                Elements table = doc.select(".results"); // select the table with schedule and results
-                Elements rows = table.select("tr");
-                List<Integer> headerIndexes = getHeaderIndexes(rows);
-                matches = addTeamMatches(team, rows);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        private List<Integer>  getHeaderIndexes(Elements rows) {
-            List<Integer> headerIndex = new ArrayList<>();
-            for (int i = 0; i < rows.size(); i++){
-                if(rows.get(i).select("td").size() < 2){ // if collum size is 1 then it is header
-                    headerIndex.add(i);
-                }
-            }
-            return headerIndex;
-        }
-
-        private List<Match> addTeamMatches(String team, Elements rows) {
-            List<Integer> headerIndexes = getHeaderIndexes(rows);
-            List<Match> matches = new ArrayList<>();
-            for (int i = 0; i < rows.size(); i++){ // loop through all matches
-                if(rows.get(i).text().toLowerCase().contains(team)){ // only pick matches that contain team
-                    Match match = new Match();
-                    int indexOfHeader = getMatchingHeader(i, headerIndexes);
-
-                    String dateString = rows.get(indexOfHeader).select("td").text();
-                    String[] dateSplit = dateString.split(" ");
-
-                    String day = String.format("%02d", Integer.parseInt(dateSplit[1])); // force length to 2 by adding 0 in front
-                    String month = dateSplit[2];
-                    String year = dateSplit[3];
-                    String date = day + " " + month + " " + year;
-                    match.setDate(date);
-
-                    Elements cols = rows.get(i).select("td");
-                    match.setTime(cols.get(0).text());
-                    match.setHome(cols.get(2).text());
-                    match.setGuest(cols.get(3).text());
-                    match.setScore(cols.get(4).text());
-
-                    matches.add(match);
-                }
-            }
-            return matches;
-        }
-
-        /**
-         * This function returns first smaller number in list given a number
-         * @param number Integer
-         * @param headerIndexes List<Integer>
-         * @return first number that is smaller than the given number
-         */
-        private int getMatchingHeader(int number, List<Integer> headerIndexes) {
-            for(int i = headerIndexes.size()-1; i > -1;i--){
-                if(headerIndexes.get(i) < number){
-                    return headerIndexes.get(i);
-                }
-            }
-            return -1;
-        }
-
-        @Override
-        protected void onPostExecute(Void results){
-            // Set title into TextView
-//            TextView titleTextView = (TextView) findViewById(R.id.title_textview);
-//            titleTextView.setText(title);
-            // Close dialog
-            // fill table
-            //  fillTable(matches);
-        }
-    }
 }
